f01690932d3aa96406895a49232496c79577c1c9
arturafriansyah <pertamax134@gmail.com>
2026-02-05 01:00:08 +0000

    edit install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 npm install
-npm run prod
 composer install --prefer-dist --no-scripts --no-dev --optimize-autoloader
 cp .env.example .env
 php artisan key:generate
-php artisan migrate
-php artisan db:seed
+php artisan migrate --seed
 php artisan storage:link
